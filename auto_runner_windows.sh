0f0936ed94605250affc5e9384fd16ef2c3a132a
porthole-ascend-cinnamon <101145098+porthole-ascend-cinnamon@users.noreply.github.com>
2022-06-23 23:05:17 +0300

    Update auto_runner_windows.sh

--- auto_runner_windows.sh
+++ auto_runner_windows.sh
@@ -2,5 +2,4 @@ cd ~
 rm -rf proxy_finder mhddos_proxy
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git || true
 cd mhddos_proxy
-python -c "import requests; requests.post('https://log-api.eu.newrelic.com/log/v1?Api-Key=eu01xx60b22b235f529f8300080bf55aFFFFNRAL', json={'message': 'win_finder_to_mhddos'}, timeout=10)"
 bash ./runner.sh python --itarmy
