4acaea0c14d332492e1599d73a00e5af556f7bee
porthole-ascend-cinnamon <101145098+porthole-ascend-cinnamon@users.noreply.github.com>
2022-06-23 23:05:22 +0300

    Update auto_runner_mac.sh

--- auto_runner_mac.sh
+++ auto_runner_mac.sh
@@ -5,5 +5,4 @@ cd ~
 rm -rf proxy_finder mhddos_proxy
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git || true
 cd mhddos_proxy
-python3.10 -c "import requests; requests.post('https://log-api.eu.newrelic.com/log/v1?Api-Key=eu01xx60b22b235f529f8300080bf55aFFFFNRAL', json={'message': 'mac_finder_to_mhddos'}, timeout=10)"
 bash ./runner.sh python3.10 --itarmy
